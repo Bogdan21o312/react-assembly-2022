e3a9117d5d147337d3ad031cf7fd6af9217d669d
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-21 22:39:39 +0300

    We output test data from the server + check the operation of loading and error processing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {testAPI} from "./sevices/test";
 
 const App = () => {
+    const {data: test, isLoading, error} = testAPI.useFetchAllTestQuery('')
+
     return (
         <div>
-          Start
+            {isLoading && <h1>Loading...</h1>}
+            {error && <h2>Error...</h2>}
+            {test && test.map(test =>
+                <div key={test.id}>
+                    {test.id}
+                    {test.body}
+                </div>
+            )}
+
         </div>
     );
 };
